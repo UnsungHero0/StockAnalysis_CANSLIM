387a99207896dad4d7a9243e19908bb93d0a4cbc
vdaytona <daytonaviola@gmail.com>
2014-09-07 11:50:10 +1000

    update

--- StockAnalysis_CANSLIM/src/main/tokyo/rawdata/database/impl/download/HistoricalQuoteDownload.java
+++ StockAnalysis_CANSLIM/src/main/tokyo/rawdata/database/impl/download/HistoricalQuoteDownload.java
@@ -96,7 +96,7 @@ class updateThread extends Thread {
 			}
 		}
 		while (ifHasNext == true) {
-			if (Integer.valueOf(code) >= 6809) {
+			if (Integer.valueOf(code) >= 6852) {
 				CreateQuotesTableFromUrl create = new CreateQuotesTableFromUrl();
 				create.createNewQuotesTable(code,
 						HistoricalQuoteDownload.con);
